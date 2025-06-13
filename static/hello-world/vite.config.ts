8772f850a3a4460cd69f44d56357f107551f82d7
Denzel Baldacchino <dbaldacchino@onpointserv.com>
2025-06-13 11:33:02 +0200

    first commit

---
+++ static/hello-world/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
